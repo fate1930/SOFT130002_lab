76092f1a64af30ce39812a66237a52b387e844d3
19302010023 <971497103@qq.com>
2020-05-27 20:43:58 +0800

    lab9

---
+++ lab9/lab9-exercise1.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Exercise 1</title>
+</head>
+<body>
+<h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
+<p>You can type regular HTML here and it will show up</p>
+
+<h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
+<?php
+//this is a php comment IN tags (will not appear)
+//$date = date("l , M dS , Y H:i:s");
+//echo "This page was generated: " . $date . "<hr/>";
+//notice we must echo tags in php.
+$remaining = 365 + date("L") - date("z");
+echo "There are ". $remaining . " days left in the year";
+?>
+</body>
+</html>
