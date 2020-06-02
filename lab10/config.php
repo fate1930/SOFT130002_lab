dd6c430e76b2c4751fd57e09b27c5ab0b1da49da
19302010023 <971497103@qq.com>
2020-06-02 17:49:13 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testuser');
+define('DBPASS', 'my=22310=wa');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
